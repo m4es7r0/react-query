b08b067ae2ec6899f5053037163ba8a7d08174e5
Musiichenko Dmitry <dmitrymusiichenko@gmail.com>
2022-11-05 20:30:28 +0200

    Initial Query Data, not cached

--- src/hook/superHeroData.js
+++ src/hook/superHeroData.js
@@ -1,13 +1,21 @@
-import { useQuery } from "react-query"
-import axios from "axios"
+import { useQuery, useQueryClient } from "react-query";
+import axios from "axios";
 
 const fetch = ({ queryKey }) => {
-    return axios.get(`http://localhost:4000/superheroes/${queryKey[1]}`)
-}
+  return axios.get(`http://localhost:4000/superheroes/${queryKey[1]}`);
+};
 
 export const useSuperHeroData = (heroId) => {
-    return useQuery(['super-hero', heroId], fetch, {
-        cacheTime: 30000,
-        select: ({ data }) => data
-    })
-}
+  const client = useQueryClient();
+  return useQuery(["super-hero", heroId], fetch, {
+    cacheTime: 30000,
+    select: ({ data }) => data,
+    initialData: () => {
+      const hero = client
+        .getQueryData("super-heroes")
+        ?.data?.find((hero) => hero.id === parseInt(heroId));
+        if(hero) return {data: hero}
+        else return undefined
+    },
+  });
+};
